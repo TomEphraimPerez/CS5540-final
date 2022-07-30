dc1d3e94e4893c33bae85baa663d40f774d25c26
tperez35 <tperez35@gatech.edu>
2022-07-29 17:09:34 -0700

    try #10

--- client.py
+++ client.py
@@ -1,26 +1,57 @@
 # ClIENT:
 
-import os, sys												# 
+# $ ssh root@allesrebel.com   # $ exit -> Connection to allesrebel.com closed.
+# $ cd 5540-final
+# $ python3 server.py	-> About to listen on http://localhost:8000
+# [blinking cursor]
+
+from ipaddress import IPv6Interface, ip_address, ip_interface, ip_network #
+import ipaddress										#
+import os, sys											# 
 import hashlib
-import requests												# 
+from telnetlib import IP
+import requests											# 
 import socket
 BUFFER_SIZE = 1024
  	
 # url = '127.0.0.1:443'		# lcl host test only, or 127.0.0.0/8 or 8.8.8.8 
 print('\nsecret (fromTempLiteral):')										
 hashedSecret = hashlib.sha224(b"314159265358979323846").hexdigest()		# test
-print(hashedSecret)											# test
-														
-host = "localhost"
-port = 8000			
+print(hashedSecret)										# test
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 		#  
+host = "localhost"										# o
+port = 8000
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # o
+s = socket.socket() 									# Either "s =..." , same results"
+print ("\nSocket successfully created\n")
+s.bind((host, port))									# OK											
+# host = socket.gethostname()
+# s.connect((host, port))			# -> ConnectionRefusedError: [Errno 61] Connection refused
+s.listen(5)  
+print ("\nsocket is listening")
+
+
+while True: 
+# Establish connection with client.
+  c, addr = s.accept()    
+  print ('Got connection from', host) 
+  # send a thank you message to the client. encoding to send byte type.
+  c.send('Thank you for connecting'.encode())
+  # Close the connection with the client
+  c.close()   
+  # Breaking once connection closed
+  break
+
+
 s.bind((host, port))
+s.close()
 
-print("\n\nCONNECTION FROM (inThisCase):", str('45.79.84.107'))
+print("\n\nCONNECTION FROM: \n", host)
+print("CONNECTION FROM: \n", port)
+print('\n')
 
 # Extract elements/features  of the server-side (from a dictionary).  
-	#  eg., from root@localhost:~# cd 5540-final / server.py / [listening on localhost:8000] / [my IP]: 		
+	# eg.,from root@localhost:~# cd 5540-final / server.py / [listening on localhost:8000]/[my IP]: 		
 		# 		c_csuites (cipher)
 		# 		c_curves (characteristic math algo)
 		# 		s_proto
@@ -28,36 +59,47 @@ print("\n\nCONNECTION FROM (inThisCase):", str('45.79.84.107'))
 		# 		c_port (client) 		
 		# 		s_ip
 		# 		tcp_rtt
-print('\n')
 serverdict = {'c_csuites' :  '_' , 	 'c_curves' :  '_' ,	 's_proto' :	 '_' ,	  's_session' :  '_' ,	 'c_port' :  '_' ,	 's_ip' :  '_'  ,	 'tcp_rtt' : '_'}	
 
-print("Original svr dict: " + str(serverdict))				#  test
+print("Original svr dict: " + str(serverdict))			#  test
 
 # https://pythonguides.com/get-all-values-from-a-dictionary-python/
-result = list(serverdict.values())							# Extract values using py built-in values().
-print("Result : " +  str(result))							# test
+result = list(serverdict.values())						# Extract values w/ py built-in values().
+print("result = " +  str(result))						# test
 print('\n')
 
 # https://docs.python.org/3/library/hashlib.html
-m = hashlib.sha224()										# 
+m = hashlib.sha224()									# †
 m.update(b"\n\n\t\tIs anyone $HOME ?\n")
 m.digest()
-print(m)
+print(m)		# -> <sha224 _hashlib.HASH object @ 0x102858350>
 
-# hashlib.sha224(result).hexdigest()						# Non-op.
+# hashlib.sha224(result).hexdigest()					# Non-op. But uneccessary since †
 digestSZ = m.digest_size
 print('digest size: ', digestSZ)
 blockSZ = m.block_size
 print('block size: ', blockSZ)
 print('\n')
 
-# NOTES ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# host = root@allesrebel.com	-  45.79.84.107 			# 							   
-	# ( server needs to worry about using a free port, and the clients need to know what this port is, 
-		# OW they'll not be able to connect to  svr. **
+
+# SEND HASHED_SECRET -------------|
+s.connect(( host, port ))								# -> OSError: Invalid argument
+s.sendall(hashedSecret.encode())   						# -> OSError: Socket is not connected
+# --------------------------------|
+
+
+
+# SEND RESULT ====================|
+
+# ================================|
+
+# NOTES ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# host = root@allesrebel.com	-  45.79.84.107 ping OK.						   
+	# (Only the server needs to worry about using a free port, & clients need to know what the port is 
+		# OW they'll not be able to connect to  svr.
 # host = "45.79.84.107"										# S-ip
-# TCP sockets are bi-directional. After connection there's no difference bt svr & client, you only have 2 
-	# ends of a stream:
-# ((My full path:  /Users/thomasperez/5540Smr22Team/GroupProject1/phoca/SendHashToSvr.txt))
+# (TCP sockets are bi-directional. After connection there's no diff bt svr & client, you only have 2 
+	# ends of a stream)
+# (My full path:  /Users/thomasperez/5540Smr22Team/GroupProject1/phoca/SendHashToSvr.txt)
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   END NOTES  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
