2b03f34b98fe25762c611f6bf16185d43a82bec7
tperez35 <tperez35@gatech.edu>
2022-07-31 20:44:49 -0700

    hopefully fixed sending hash in byte-form

--- client.py
+++ client.py
@@ -1,10 +1,8 @@
 import http.client
-<<<<<<< HEAD
 import socket, json                                         #
 import hashlib
-=======
 import json
->>>>>>> e4766f40da41b6078081595b4837b5c88e1748aa
+import socket
 
 print('\nConnection: ')
 connection = http.client.HTTPConnection("localhost", 8000)
@@ -27,8 +25,6 @@ print(response.read().decode())
 # print("Status: {} and reason: {}".format(response.status, response.reason))
 # print(response.read().decode())
 
-<<<<<<< HEAD
-
 # ==============================================================================================|
 serverdict = {'c_csuites': '_', 'c_curves': '_', 's_proto': '_', 's_session': '_', 'c_port': '_',
             's_ip': '_', 'tcp_rtt': '_'}
@@ -36,23 +32,36 @@ print("Original svr dict: " + str(serverdict))      # '_' py placeholders
 print('\n')
 result = list(serverdict.values())
 print("result = " + str(result))
-
-
-# hashedSecret = hashlib.sha224(b"314159265358979323846").hexdigest()         # o # compiles but...
-hashedSecret = hashlib.sha224(bytes('314159265358979323846'.encode()))    # compiles but...
-
-print('\nhashedSecret: ')
-print(hashedSecret)
+# hashedSecret = hashlib.sha224(b"314159265358979323846").hexdigest()     # o # compiles but...
+# hashedSecret = hashlib.sha224(bytes('314159265358979323846'.encode()))    # compiles but...
+                                        # or
+# hashedSecret = hashlib.md5()
+# update_bytes = b'314159265358979323846'
+# hashedSecret.update(update_bytes)
+# print('\nhashedSecret: ')
+# print(hashedSecret)
 print('\n')
-
-connection.send(hashedSecret)
+# connection.send(hashedSecret)
 # connection.send(hashedSecret)  #TypeError:bytes-like obj required not 'str' nor '_hashlib.HASH'
-
 # =============================================================================================|
 
 
+
+
+print("Massage to pass: ")
+msg = input("314159265358979323846")
+msg_utf = str.encode(msg)
+hashedSecret = hashlib.sha256(msg_utf)
+print('\nHash object: ')
+print(hashedSecret)
+#hex_dig = hashedSecret.hexdigest()
+# message_to_send = "{" + msg + ";" + hashedSecret + "}"
+message_to_send = "{" + msg + " + ";"}"
+# val_bytes = bytearray(hashedSecret.digest())
+# connection.send(val_bytes)
+# data = connection.recv(1024).decode()
+# print(data)
+#msg = input()
 print('\n')
 connection.close()
-=======
-connection.close()
->>>>>>> e4766f40da41b6078081595b4837b5c88e1748aa
+
